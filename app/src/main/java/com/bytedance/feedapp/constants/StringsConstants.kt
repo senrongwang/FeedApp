ca1d1e2748a0cba2214e95bdcf2b65fd70aecfc9
wsr <1693526376@qq.com>
2025-11-19 17:36:55 +0800

    修改常量类名称

---
+++ app/src/main/java/com/bytedance/feedapp/constants/StringsConstants.kt
@@ -0,0 +1,12 @@
+package com.bytedance.feedapp.constants
+
+object StringsConstants {
+    const val SEARCH_TEXT_PLACEHOLDER = "点击输入搜索内容"
+    const val BACK_BUTTON_CONTENT_DESCRIPTION = "Back"
+    const val SEARCH_BUTTON_TEXT = "搜索"
+    val TABS = listOf("综合", "视频", "用户", "图文", "商品")
+
+    const val REFRESH_INFO = "刷新成功"
+    const val LOADING_MORE = "正在加载..."
+    const val NO_MORE_DATA = "没有更多内容了"
+}
