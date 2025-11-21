90eed588ea7c29e426eae8c71488f65c9a7a37aa
wsr <1693526376@qq.com>
2025-11-21 18:08:39 +0800

    卡片曝光代码开发

--- app/src/main/java/com/bytedance/feedapp/listen/ExposureState.kt
+++
@@ -1,8 +0,0 @@
-package com.bytedance.feedapp.listen
-
-data class ExposureState(
-    var isExposed: Boolean = false,
-    var is50PercentExposed: Boolean = false,
-    var isFullyExposed: Boolean = false,
-    var isDisappeared: Boolean = true // Initially, all cards are considered not visible
-)
