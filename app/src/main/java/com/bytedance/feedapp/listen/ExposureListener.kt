b87431d370823c683cbc46be34b8fa5f748bbb5a
wsr <1693526376@qq.com>
2025-11-21 18:08:39 +0800

    卡片曝光代码开发

--- app/src/main/java/com/bytedance/feedapp/listen/ExposureListener.kt
+++
@@ -1,8 +0,0 @@
-package com.bytedance.feedapp.listen
-
-interface ExposureListener {
-    fun onCardExposed(position: Int)
-    fun onCardExposed50Percent(position: Int)
-    fun onCardFullyExposed(position: Int)
-    fun onCardDisappeared(position: Int)
-}
